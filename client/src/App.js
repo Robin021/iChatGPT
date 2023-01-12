3d8f1cb132c8a150a0f4aada364c98164cebc0da
robin021 <westman021@qq.com>
2023-01-12 10:05:46 +0800

    change

--- client/src/App.js
+++ client/src/App.js
@@ -21,7 +21,7 @@ function App() {
       if (questions.length === 0) {
         return;
       }
-      let response = await fetch(`http://127.0.0.1:5000/ask?q=${questions[0]}&conversation_id=${conversationId}`); 
+      let response = await fetch(`http://127.0.0.1:5001/ask?q=${questions[0]}&conversation_id=${conversationId}`); 
       response = await response.json();
       setAnswers([...answers, response.answers]);
       setQuestions([]);  
@@ -32,7 +32,7 @@ function App() {
   }, [questions]);
   useEffect(() => {
     const getNewConversationId = async () => {
-      let response = await fetch(`http://127.0.0.1:5000/new-conversation`);
+      let response = await fetch(`http://127.0.0.1:5001/new-conversation`);
       response = await response.json();
       const id = response.id;
       setConversationId(id); 
--- server/server.py
+++ server/server.py
@@ -64,4 +64,4 @@ def new_conversation():
     return {"id": id}
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host="0.0.0.0",port=5000,debug=True)
