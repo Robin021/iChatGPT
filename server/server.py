feaf953acc6ed20d0fcb372ac51a5ba162d4d99b
robin021 <westman021@qq.com>
2023-01-18 23:54:35 +0800

    change some style related

--- server/server.py
+++ server/server.py
@@ -32,14 +32,15 @@ def ask():
     history.append(prompt)  # add the latest question to the history
     completions = openai.Completion.create(
         engine="text-davinci-003",
-        prompt='\n'.join(history),  # pass the entire history as the prompt
-        max_tokens=1024,
+        prompt = ' '.join(history),
+        max_tokens=2048,
         n=1,
         stop=None,
-        temperature=0.5
+        temperature=0.4
     )
     id = completions.id
     message = completions.choices[0].text
+    message = message.replace("\n\n", "")
     history.append(message)
 
     print(message)
