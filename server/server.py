dd5e7b51944633824bf94ab8cc0a1b635acbb645
robin021 <westman021@qq.com>
2023-02-13 15:41:41 +0800

    add wx

--- server/server.py
+++ server/server.py
@@ -67,12 +67,60 @@ def chat():
     # print(message)
     return {"answers": message}
 
-@app.route("/asr", methods=["GET"])
-@cross_origin()
-def asr():
-    file_path = "path/to/audio/file.mp3"
-    response = openai.Audio.create(file=file_path, model="text-davinci-002")
-    print(response["data"])
+@app.route('/', methods=["GET", "POST"])
+def default():
+      return "hello ,server running"
+
+@app.route("/wx", methods=["GET", "POST"])
+def index():
+    if request.method == "GET":
+        signature = request.args.get('signature')
+        timestamp = request.args.get('timestamp')
+        nonce = request.args.get('nonce')
+        echostr = request.args.get('echostr')
+        token = "021d"
+        try:
+            check_signature(token, signature, timestamp, nonce)
+        except InvalidSignatureException:
+            # 处理异常情况或忽略
+            return "校验失败"
+        # 校验成功
+        return echostr
+    if request.method == "POST":
+        xml_str = request.data
+        # 解析xml格式数据
+        msg = parse_message(xml_str)
+        # 1.目标用户信息
+        target = msg.target
+        # 2.发送用户信息
+        source = msg.source
+        # 3.消息类型
+        msgType = msg.type
+        # 4.消息内容
+        msgCcontent = msg.content
+
+        print(msgCcontent)
+
+        reply = TextReply()
+        reply.source = target
+        reply.target = source
+
+        # Start chat
+        PROMPT = user_request
+        # print("prompt:" + PROMPT)
+        if PROMPT.startswith("!"):
+            if chatbot_commands(PROMPT):
+                print("continue")
+        response = chatbot.ask(PROMPT)
+        # print("ChatGPT: " + response["choices"][0]["text"])
+        logger.info("接收微信消息->\n"+str(msgCcontent))
+        # response = requests.post('https://api.openai.com/v1/completions', headers=headers, json=json_data)
+        reply.content = response["choices"][0]["text"].strip().replace("\n", "")
+        logger.info("返回微信消息->\n"+str(reply.content))
+        print(reply.content)
+        # 包装成XML格式的数据
+        xml = reply.render()
+        return xml
 
 
 if __name__ == "__main__":
